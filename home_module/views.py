e1bec57856ccbbf6bba970ed7cd788e5cc54b94a
Alireza Keshavarz <alir4akeshavrz@gmail.com>
2023-03-27 20:45:02 +0430

    add send email settings and forget&reset password features

--- home_module/views.py
+++ home_module/views.py
@@ -1,10 +1,14 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 from django.views import View
 from django.contrib.auth import login, logout
 
-from home_module.forms import UserRegisterForm, UserLoginForm
+from home_module.forms import UserRegisterForm, UserLoginForm, ForgetPasswordForm, ResetPasswordForm, \
+    VerifyForgetPasswordForm
 from home_module.models import User
 
 
@@ -68,3 +72,67 @@ class UserLogOutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         return redirect('home_module:index')
+
+
+class ForgetPasswordView(View):
+    form_class = ForgetPasswordForm
+    template_name = ''
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            msg = get_random_string(72)
+            request.session['forget_code'] = msg
+            request.session.save()
+            send_mail('Reset Password', msg, settings.EMAIL_HOST_USER, [email, ], fail_silently=False)
+            # todo: redirect to reset pass page
+            return redirect('')
+        return render(request, self.template_name, {'form': form})
+
+
+class VerifyForgetPasswordView(View):
+    template_name = ''
+    form_class = VerifyForgetPasswordForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['code'] == request.session['forget_code']:
+                request.session['reset-pass-true'] = True
+                return redirect('')
+            else:
+                messages.error(request, 'invalid code', 'danger')
+        return render(request, self.template_name, {'form': form})
+
+
+class ResetPasswordView(View):
+    template_name = ''
+    form_class = ResetPasswordForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.session['reset-pass-true']:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            return redirect('home_module:index')
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid:
+            request.user.set_password(form.cleaned_data['password'])
+            request.user.save()
+            messages.success(request, 'successfully changed', 'success')
+            return redirect('home_module:log_in')
+        return render(request, self.template_name, {'form': form})
